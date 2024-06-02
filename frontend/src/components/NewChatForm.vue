de3a1aa1adc4a17c374702c2b4beb27e1cc803ab
tkogane0626 <pad.kokin@gmail.com>
2024-06-02 17:28:50 +0900

    リアルタイムチャット機能の実装

---
+++ frontend/src/components/NewChatForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <form>
+    <textarea
+      v-model="message"
+      @keypress.enter.prevent="handleSubmit"
+      placeholder="メッセージを入れてEnterを押してください"
+    ></textarea>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: [
+    'connectCable'
+  ],
+  data() {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit('connectCable', this.message)
+      this.message = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  margin: 10px;
+}
+
+textarea {
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 6px;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 0;
+  border-radius: 20px;
+  font-family: inherit;
+  outline: none;
+}
+</style>
--- frontend/src/views/Chatroom.vue
+++ frontend/src/views/Chatroom.vue
@@ -1,15 +1,70 @@
 <template>
   <div class="container">
     <Navbar />
+    <ChatWindow :messages="messages" />
+    <NewChatForm @connectCable="connectCable" />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import ActionCable from 'actioncable'
 import Navbar from '../components/Navbar.vue'
+import ChatWindow from '../components/ChatWindow.vue'
+import NewChatForm from '../components/NewChatForm.vue';
+import { getAccessToken, getClient, getUid } from '../auth/getItem'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    ChatWindow,
+    NewChatForm
+  },
+  data() {
+    return {
+      messages: []
+    }
+  },
+  methods: {
+    async getMessages() {
+      try {
+        const res = await axios.get('http://localhost:3000/messages', {
+          headers: {
+            "access-token": getAccessToken(),
+            client: getClient(),
+            uid: getUid()
+          }
+        })
+
+        if (!res) {
+          new Error('メッセージ一覧を取得できませんでした')
+        }
+
+        this.messages = res.data
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    connectCable(message) {
+      this.messageChannel.perform('receive', {
+        message: message,
+        email: getUid()
+      })
+    }
+  },
+  mounted () {
+    const cable = ActionCable.createConsumer('ws://localhost:3000/cable')
+    this.messageChannel = cable.subscriptions.create('RoomChannel', {
+      connected: () => {
+        this.getMessages()
+      },
+      received: () => {
+        this.getMessages()
+      }
+    })
+  },
+  beforeUnmount() {
+    this.messageChannel.unsubscribe()
   }
 }
 </script>
