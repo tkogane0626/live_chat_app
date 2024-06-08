94928f73a28283db151722bdb576b8cbc76cfd27
tkogane0626 <pad.kokin@gmail.com>
2024-06-08 21:07:27 +0900

    いいねのリアルタイム反映機能の修正

--- backend/app/channels/room_channel.rb
+++ backend/app/channels/room_channel.rb
@@ -9,9 +9,23 @@ class RoomChannel < ApplicationCable::Channel
 
   def receive(data)
     user = User.find_by(email: data['email'])
+    data_message = data['message']
 
-    return unless (message = Message.create!(content: data['message'], user_id: user.id))
+    if data_message.present?
+      begin
+        message = Message.create!(content: data_message, user_id: user.id)
+      rescue StandardError => e
+        Rails.logger.error(e)
+      end
+    end
 
-    ActionCable.server.broadcast('room_channel', { message: data['message'], name: user.name, created_at: message.created_at })
+    ActionCable.server.broadcast(
+      'room_channel',
+      {
+        message: data_message,
+        name: user.name,
+        created_at: message&.created_at
+      }
+    )
   end
 end
